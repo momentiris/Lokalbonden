23fb1b8743cc17503fbf05f14f9cf8143b793f9d
Maja Persson <persson.maja@gmail.com>
2018-02-22 17:03:29 +0100

    Initial commit

---
+++ webpack.mix.js
@@ -0,0 +1,24 @@
+const mix = require('laravel-mix');
+
+require('dotenv').config();
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+const theme = process.env.WP_THEME;
+
+mix.setResourceRoot('../');
+mix.setPublicPath(`public/themes/${theme}/assets`);
+
+mix.js('resources/assets/scripts/app.js', 'scripts');
+mix.sass('resources/assets/styles/app.scss', 'styles');
+
+mix.version();
