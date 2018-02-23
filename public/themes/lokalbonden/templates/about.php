fbc3df4fef8461190ff328093516c4ed31ebad9d
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2018-02-23 16:35:04 +0100

    added about.php and function to get posts from category

---
+++ public/themes/lokalbonden/templates/about.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: About
+*/
+// Get posts from category.
+// $aboutPosts = get_posts(array('category_name' => 'about'));
+// var_dump($aboutPosts);
+
+
+
+?>
+<?php get_header(); ?>
+
+<div id="main-content" class="main-content">
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php get_footer();?>
