5cd15e1a2a3d5adc5f411f703778467256901f60
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2018-02-26 14:42:58 +0100

    testing acf json

--- public/themes/lokalbonden/templates/home.php
+++ public/themes/lokalbonden/templates/home.php
@@ -2,21 +2,38 @@
 /*
 Template Name: Home
 */
-get_header();
+// get_header();
 ?>
 
 <div class="hero_wrap">
-<?php $homeTitle = get_posts(array('category_name' => 'HomeTitle'));
+<?php
+// $homeTitle = get_posts(array('category_name' => 'HomeTitle'));
+// $ingress = get_field('ingress');
+$image = get_field('bakgrundsbild');
+$size = 'full'; // (thumbnail, medium, large, full or custom size)
+
+if( $image ) {
+
+	echo wp_get_attachment_image( $image, $size );
+
+}
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo '<br>';
+var_dump($image['url']);
 
     // Loops the posts with our defined search parameters
-    foreach ( $homeTitle as $post ) : setup_postdata( $post ); ?>
+    // foreach ( $ingress as $items ) : setup_postdata( $items ); ?>
 
-    <h1 class="home-title"><?php the_content(); ?></h1>                      <!-- Get post content -->
-
-<?php endforeach;
+    <!-- <h1 class="home-title"><?php //echo $ingress ?></h1> -->
+              <!-- Get post content -->
+<!-- <?php //endforeach; -->
 
     // Resets post searching parameters to default
-    wp_reset_postdata();?>
+    // wp_reset_postdata();?>
 </div>
 <div class="home-bridge">
     <h2>En familjär matkasse</h2>
