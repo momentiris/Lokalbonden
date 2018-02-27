9174e7558add20bfa8bd6c3e650215c93ca09631
Maja Persson <persson.maja@gmail.com>
2018-02-27 15:08:51 +0100

    Fixed merge error in home template

--- public/themes/lokalbonden/templates/home.php
+++ public/themes/lokalbonden/templates/home.php
@@ -3,21 +3,8 @@
 Template Name: Home
 */
 get_header();
-<<<<<<< HEAD
-?>
-
-<div class="hero_wrap">
-<?php
-// $homeTitle = get_posts(array('category_name' => 'HomeTitle'));
-// $ingress = get_field('ingress');
-$image = get_field('bakgrundsbild');
-$size = 'full'; // (thumbnail, medium, large, full or custom size)
-=======
->>>>>>> b7cff20d4b1863e2690d3b1f803c40c65d0d9eef
-
 $fields = get_fields();
 $image = $fields['background_image_1'];
-
 ?>
 
 <div class="hero_wrap" style="background-image: url('<?php echo $image['sizes']['large'] ?>'">
