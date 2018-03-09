a7fc7c38a6730fe4c95a157d238245734756f8ab
Maja Persson <persson.maja@gmail.com>
2018-03-09 13:05:30 +0100

    Started working with new svg

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -1,5 +1,3 @@
 import hamburger from './hamburger.js';
-import menuSmall from './menu.js';
+import './menu.js';
 import './backgroundswap.js';
-
-window.addEventListener('scroll', menuSmall);
--- resources/assets/scripts/menu.js
+++ resources/assets/scripts/menu.js
@@ -1,8 +1,19 @@
-export default function menuSmall(e) {
-  const menu = document.querySelector('.menu-navbar-container');
+export default (function () {
+  function menuSmall(e) {
+  const menu = document.querySelector('.menu-container');
   if (window.scrollY > menu.clientHeight) {
     menu.classList.add('small');
   } else {
     menu.classList.remove('small');
   }
 }
+
+const wobblyNav = document.querySelector('.wobbly_nav');
+const currentPage = document.querySelector('.current-menu-item')
+console.dir(wobblyNav);
+wobblyNav.clientHeight = currentPage.clientHeight;
+wobblyNav.clientWidth = currentPage.clientWidth;
+
+window.addEventListener('scroll', menuSmall);
+
+})();
