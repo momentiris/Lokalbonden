08777278107bed8b09217dc069bf58c11a9f2460
Maja Persson <persson.maja@gmail.com>
2018-03-12 16:15:43 +0100

    Added responsive menu

--- resources/assets/scripts/hamburger.js
+++ resources/assets/scripts/hamburger.js
@@ -1,5 +1,7 @@
-  // const hamburger = document.querySelector('.hamburger');
-  //
-  // hamburger.addEventListener('click', () => {
-  //   hamburger.classList.toggle('is-active');
-  // })
+  const hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+    hamburger.nextElementSibling.classList.toggle('is-open');
+    hamburger.parentElement.classList.toggle('is-open');
+  })
