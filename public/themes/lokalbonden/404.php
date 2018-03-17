a1eb915c5667de3173c453dcb16630df55347332
cjcmattsson <cjcmattsson@gmail.com>
2018-03-17 19:36:12 +0100

    Created 404-page

---
+++ public/themes/lokalbonden/404.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package WordPress
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area error-page">
+		<div id="content" class="site-content error-page-content" role="main">
+
+			<header class="page-header">
+				<h1 class="page-title">Ojdå!</h1>
+			</header>
+
+			<div class="page-wrapper">
+				<div class="page-content">
+					<p class="error-message">Den här sidan verkar inte ha mognat ännu...</p>
+				</div><!-- .page-content -->
+			</div><!-- .page-wrapper -->
+      <a class="button" href="/">Gå tillbaka startsidan!</a>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
