ada143b6fb2a42038a897398b072782083ffd2b4
krisKurken <kristian.liljander@live.se>
2018-04-10 10:38:53 +0200

    Om oss lek

--- public/themes/lokalbonden/custom_post_types/customposttypes.php
+++ public/themes/lokalbonden/custom_post_types/customposttypes.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of Lokalbonden.
- *
- * MIT License, Lokalbonden special squad.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 $customPostTypes = [
     ['Clients', 'Client'],
     ['Orders', 'Order'],
--- resources/assets/scripts/aboutAnimations.js
+++ resources/assets/scripts/aboutAnimations.js
@@ -2,27 +2,28 @@ if (window.location.pathname == "/om-oss/") {
 function isElementInViewport(el) {
   var rect = el.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
+    rect.top+rect.height/2 >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document. documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document. documentElement.clientWidth)
+    rect.bottom-rect.height/2 <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 };
 
-function addListner(elem, tagetClass, addClass){
+function addListner(elem, addClass){
   window.addEventListener('scroll', ()=>{
     if (isElementInViewport(elem[0]) && elem[1] == false) {
-      elem[0].querySelector(`.${tagetClass}`).classList.add(`${addClass}`);
+      elem[0].classList.add(`${addClass}`);
       elem[1] = true;
+      console.log('true');
     }
   });
 }
 
-let elem_section1 = [document.querySelector('.first_section'), false];
-let elem_section2 = [document.querySelector('.second_section'), false];
-let section_border = [document.querySelector('.section_border_wrapper'), false];
+let elem_section1 = [document.querySelector('.first_section .svg_wrap'), false];
+let elem_section2 = [document.querySelector('.second_section .svg_wrap'), false];
+let section_border = [document.querySelector('.section_border'), false];
 
-addListner(elem_section1, 'svg_wrap', 'svg_wrap_active');
-addListner(elem_section2, 'svg_wrap', 'svg_wrap_active');
-addListner(section_border, 'section_border', 'section_border_active');
+addListner(elem_section1, 'svg_wrap_active');
+addListner(elem_section2, 'svg_wrap_active');
+addListner(section_border, 'section_border_active');
 };
