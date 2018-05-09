8faa805e2c8b251482fe797300fb3d1d6ad5c5ff
cjcmattsson <cjcmattsson@gmail.com>
2018-05-09 20:37:05 +0200

    Fixed padding on Om Oss-page, changed favicon and added picture for Säsongskassen

--- public/themes/lokalbonden/functions.php
+++ public/themes/lokalbonden/functions.php
@@ -127,10 +127,10 @@ array(
 )
 );
 
-// Adding fontawesome icons
+// Adding fontawesome icons FRO CARTICON
+// NOT USED RIGHT NOW
 function wmpudev_enqueue_icon_stylesheet() {
 wp_register_style( 'fontawesome', 'http:////maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
 wp_enqueue_style( 'fontawesome');
 }
 add_action( 'wp_enqueue_scripts', 'wmpudev_enqueue_icon_stylesheet' );
-
