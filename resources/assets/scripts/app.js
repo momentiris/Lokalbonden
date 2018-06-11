81cdfc852625e7ffb6a62a5f868c35d721d341e9
cjcmattsson <cjcmattsson@gmail.com>
2018-06-11 17:45:21 +0200

    Fixed stuff that Martin asked for

--- resources/assets/scripts/app.js
+++ resources/assets/scripts/app.js
@@ -3,6 +3,5 @@ import './menu.js';
 import './backgroundswap.js';
 import observeWobble from './home_wobbly_line.js';
 import './cart.js';
-import './googleMap';
 import './aboutAnimations';
 import './swiper_home.js';
